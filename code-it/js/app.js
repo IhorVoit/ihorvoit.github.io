753b2050d8605b7f191b3b8dcac303e30c4c12ab
IhorVoit <ihorvoit12@gmail.com>
2018-12-27 00:03:14 +0200

    commit code-it test

---
+++ code-it/js/app.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+ $('.advantages').slick({
+  slidesToShow: 4,
+  arrows: false,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 3
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 2
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1
+    }
+  }
+
+  ]
+});
+ $('.menu_btn').on('click', function(){
+  $(this).toggleClass('open');
+  $('ul.nav').toggleClass('open');
+});
+
+});
+
+
+
+//# sourceMappingURL=../maps/app.js.map
