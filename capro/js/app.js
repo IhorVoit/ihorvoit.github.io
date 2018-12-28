9f995dcfce459c0d3400592f490f85313266ad5d
IhorVoit <ihorvoit12@gmail.com>
2018-12-28 18:38:37 +0200

    fixed poligrafia slider added multiple range

--- capro/js/app.js
+++ capro/js/app.js
@@ -1,25 +1,27 @@
-<<<<<<< HEAD
 $(document).ready(function(){
   $('.slider').slick({
   	autoplay: true,
   	arrow: true,
   	dots: true
   });
-  $('.typography__slider').slick({
-    centerMode: true,
-    centerPadding: '200px',
-    slideToShow: 3,
-    slideToScroll: 1,
-    dots: true,
-    responsive: [{
-      breakpoint: 1200, 
-      settings: {
-        centerMode: false
-      }
-    }
-    ]
-  });
-
+  // $('.typography__slider').slick({
+  //   centerMode: true,
+  //   centerPadding: '200px',
+  //   slideToShow: 3,
+  //   slideToScroll: 1,
+  //   dots: true,
+  //   responsive: [{
+  //     breakpoint: 1200, 
+  //     settings: {
+  //       centerMode: false
+  //     }
+  //   }
+  //   ]
+  // });
+$('#cascade-slider').cascadeSlider({
+    itemClass: 'cascade-slider_item',
+    arrowClass: 'cascade-slider_arrow'
+});
 
   $('.our__projects span').click(function() {
   $(this).toggleClass('open');
@@ -53,6 +55,18 @@ $(document).ready(function(){
   });
   
 
+    $(".js-range-slider").ionRangeSlider({
+        type: "double",
+        min: 1000,
+        max: 10000,
+        from: 1500,
+        to: 5000,
+        step: 50,
+        grid_margin: false,
+        hide_from_to: false,
+        hide_min_max: true
+    });
+    
 });
 function showMenu(){
   if ($(window).width()>'768') {
@@ -62,70 +76,4 @@ function showMenu(){
   	$('nav.main__menu').slideUp(20);
   }
 }
-$(window).on('load resize',showMenu);
-=======
-$(document).ready(function(){
-  $('.slider').slick({
-  	autoplay: true,
-  	arrow: true,
-  	dots: true
-  });
-  $('.typography__slider').slick({
-    centerMode: true,
-    centerPadding: '200px',
-    slideToShow: 3,
-    slideToScroll: 1,
-    dots: true,
-    responsive: [{
-      breakpoint: 1200, 
-      settings: {
-        centerMode: false
-      }
-    }
-    ]
-  });
-
-
-  $('.our__projects span').click(function() {
-  $(this).toggleClass('open');
-  $(this).nextAll().slideToggle(500);
-  });
-
-  $('.filter .menu-item').click(function(e) {
-    e.preventDefault();
-    if ($(this).hasClass('active')) {
-      $(this).removeClass('active');
-      $(this).find('.toggle').html('+');
-      $(this).next('ul.submenu').slideUp(600);
-    }
-    else {
-      $(this).addClass('active');
-      $(this).find('.toggle').html('-');
-      $(this).next('ul.submenu').slideDown(600);
-    }
-  });
-
-  $('.filter__price ul .filter__price--item').click(function(e) {
-    e.preventDefault();
-    $(this).find('ul.submenu').slideToggle(600);
-  });
-
-
-  $('.main__btn__menu').click(function(e) {
-  e.preventDefault();
-  $(this).toggleClass('open');
-  $('nav.main__menu').slideToggle(1200);
-  });
-  
-
-});
-function showMenu(){
-  if ($(window).width()>'768') {
-  	$('nav.main__menu').slideDown(200);
-  } else {
-  	$('.main__btn__menu').removeClass('open');
-  	$('nav.main__menu').slideUp(20);
-  }
-}
-$(window).on('load resize',showMenu);
->>>>>>> origin/master
+$(window).on('load',showMenu);
